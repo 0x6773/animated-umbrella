84095e4fc83811ad64a13ab11812beeea3fbc8d9
Govind Sahai <gsiitbhu@gmail.com>
2016-10-28 14:43:19 +0530

    Added 0x004

---
+++ 0x400-Networking/hacking.h
@@ -0,0 +1,54 @@
+#ifndef __HACKING__H__
+#define __HACKING__H__ 1
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void dump(const char* data_buffer, const int length)
+{
+	char byte;
+	int i, j;
+	for(i = 0; i < length; ++i)
+	{
+		byte = data_buffer[i];
+		printf("%02x ", data_buffer[i]);
+		if(((i % 16) == 15) || (i == length - 1))
+		{
+			for(j = 0; j < 15 - (i % 16); ++j)
+			{
+				printf("  ");
+			}
+			printf("| ");
+			for(j = (i - (i%16)); j <= i; ++j)
+			{
+				byte = data_buffer[j];
+				if((byte > 31) && (byte < 127))
+					printf("%c", byte);
+				else
+					printf(".");
+			}
+			printf("\n");
+		}
+	}
+}
+
+void fatal(char *message)
+{
+	char error_message[100];
+	strcpy(error_message, "[!!] Fatal Error ");
+	strncat(error_message, message, 83);
+	perror(error_message);
+	exit(-1);
+}
+
+void *ec_malloc(unsigned int size)
+{
+	void *ptr;
+	ptr = malloc(size);
+	if(ptr == NULL)
+		fatal("in ec_malloc() on memory allocation");
+	return ptr;
+}
+
+#endif
